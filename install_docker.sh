b8402eeae87a41ff3029a7d1580d0dc7709b7974
Erik <erik.flaten@gmail.com>
2019-12-19 12:53:39 -0500

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -10,9 +10,10 @@ sudo apt-get install -y httping
 sudo apt-get install -y jq
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.25.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+
 sudo chmod +x /usr/local/bin/docker-compose
 
 # the username needs to be changed
 while IFS= read -r line; do
   sudo usermod -aG docker $line
-done < <( 1s -1 /users | grep 4096 | cut -d' ' -f3 )
+done < <( ls -l /users | grep 4096 | cut -d' ' -f3 )
